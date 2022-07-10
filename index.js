e134f4be57b0f538b5b127222ef7d05e153cbffc
ademclk <ademonurcelik@gmail.com>
2022-07-10 23:10:43 +0300

    JSX practice.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-// const h1 = document.createElement('h1')
+const navbar = (
+    <nav>
+        <h1>Adem's Cafe</h1>
+        <ul>
+            <li>Coffee</li>
+            <li>Desserts</li>
+            <li>Drinks</li>
+        </ul>
+    </nav>
+)
 
-// h1.textContent = "Imperative way of programming"
-// h1.className = "header"
-// document.getElementById("root").append(h1)
-
-ReactDOM.render(<h1 className="header">Declarative way of programming</h1>, document.getElementById("root"))
+ReactDOM.render(navbar, document.getElementById('root'))
