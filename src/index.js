d8a2cb3bbfa182c983fca5cc66125116c366206a
ademclk <ademonurcelik@gmail.com>
2022-07-11 11:02:08 +0300

    Added header and footer.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ import './index.css';
 function PageComponent() {
   return (
     <div>
-    <img src={require('./react-logo.png')} alt='' width="60px"/>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Is maintained by Facebook</li>
-      <li>Is used by over 1.5 million developers</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
+      <header>
+        <nav>
+          <img src={require('./react-logo.png')} alt='' width="60px"/>
+        </nav>
+      </header>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Is maintained by Facebook</li>
+        <li>Is used by over 1.5 million developers</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+      <footer>
+        <small>&copy; 2022 efreiann development. All rights reserved.</small>
+      </footer>
   </div>
   )
 }
